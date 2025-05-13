c0106da37c699d1818fa859c8aeebc47fc91df3d
Simon Körner <simon.koerner@ncore-build.de>
2025-05-13 11:35:24 +0200

    Initial commit

---
+++ app/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { styles } from "@/src/styles/Home.styles";
+import { Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Expo!</Text>
+    </View>
+  );
+};
+
+export default Home;
