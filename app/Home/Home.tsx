688fd9e278634538710e667ea7ccf7d52b916546
Simon Körner <simon.koerner@ncore-build.de>
2025-05-14 17:07:11 +0200

    basic styling

--- app/Home/Home.tsx
+++ app/Home/Home.tsx
@@ -1,10 +1,12 @@
 import { styles } from "@/src/styles/Home.styles";
+import React from "react";
 import { Text, View } from "react-native";
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello Expo!</Text>
+      <Text style={styles.title}>Hello Expo!</Text>
+      <Text style={styles.card}>Reading List App</Text>
     </View>
   );
 };
--- src/styles/Home.styles.ts
+++ src/styles/Home.styles.ts
@@ -6,4 +6,24 @@ export const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    padding: 16,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
 });
